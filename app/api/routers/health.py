b5b50d87d4816b2c3958e456b92089236f08b594
hatred <gajethatredddd@vk.com>
2025-11-29 03:26:10 +0500

    Change health check to string response

--- app/api/routers/health.py
+++ app/api/routers/health.py
@@ -4,7 +4,7 @@ router = APIRouter()
 
 @router.get("/health",tags =["Здоровье"],summary = ["Проверить жив ли сервис"],include_in_schema=True)
 async def health_check():
-    return {"status": True}
+    return {"status": "healthy"}
 
 
 @router.get("/test/division-by-zero",tags =["ОШИБКИ"],summary = ["Деление на ноль"])
